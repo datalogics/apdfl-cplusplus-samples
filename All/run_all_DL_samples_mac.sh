e1c75c949899cde91ac9a25550300205aeda8457
Rob Lyman <robl@datalogics.com>
2026-04-17 14:49:33 -0500

    Add the WebToPDF sample to the set of *all* samples run

--- All/run_all_DL_samples_mac.sh
+++ All/run_all_DL_samples_mac.sh
@@ -203,6 +203,11 @@ cd ../../DocumentConversion/ConvertPDFtoPostscript
 ./ConvertPDFtoPostscript-${stage}.app/Contents/MacOS/ConvertPDFtoPostscript-${stage}
 echo ""
 
+echo Running sample ConvertWebToPDF
+cd ../../DocumentConversion/ConvertWebToPDF
+./ConvertWebToPDF-${stage}.app/Contents/MacOS/ConvertWebToPDF-${stage}
+echo ""
+
 # FileSystem
 
 echo Running sample AlternateFileSystem
